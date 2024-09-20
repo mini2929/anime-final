9e6a75629e4d05bdb43e2c0aa5ebbcc8cc065455
mini2929 <haloyun29@gmail.com>
2024-09-20 13:29:02 +0900

    반복문으로 축약

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const [btn1, btn2, btn3] = document.querySelectorAll("button");
+const [btn1, btn2, btn3, btn4] = document.querySelectorAll("button");
 
 btn1.addEventListener("click", () => {
     new Anime(window, {scroll:0}, {duration: 500});
@@ -12,3 +12,30 @@ btn2.addEventListener("click", () => {
 btn3.addEventListener("click", () => {
     new Anime(window, {scroll: 4000}, {duration:500});
 });
+
+btn4.addEventListener("click", () => {
+    new Anime(window, {scroll: 4000}
+        , {duration:500});
+});
+
+// 위의 3개의 이벤트 구문을 반복문을 통해 하나로 축약
+
+
+/*
+내 생각
+변수로? 
+
+n=0 부터 n=변수1까지
+DOM명.addEventListener("click", () => {
+    new Anime(window, {scroll: 변수})
+    })
+*/
+
+
+const
+const posArr = [0, 2000, 4000];
+
+btns.forEach((btn, idx)) => {
+    btn.addEventListener("click", () => {
+        new Anime(window, {scroll : posArr[idx]},)}
+    )}
